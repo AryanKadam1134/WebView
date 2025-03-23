f02f1732c5b52c2021941ceb1e7cc1f2990fc391
AryanKadam1134 <aryankadam3114@gmail.com>
2025-03-23 18:28:51 +0530

    update

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
-export default function App() {
+const MyWebView = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView 
+        source={{ uri: "https://testwebapp-eight.vercel.app/" }} 
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default MyWebView;
